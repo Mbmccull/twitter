ad5fc46887a5e7dd87cbb64432eac954cff97671
Megan McCullough <megan.beth.mccullough@gmail.com>
2017-02-20 20:53:51 -0500

    adds follows and relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'profiles/:id' => 'profiles#show', as: :profile
+
+  get 'profiles' => 'profiles#index'
+
   devise_for :users
   resources :tweets
+  resources :relationships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
